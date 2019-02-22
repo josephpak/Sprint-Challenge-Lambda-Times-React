ff0f0b53707b3c877c75ccb5e7edbb71bdfa7d15
joepak <josephpak2014@gmail.com>
2019-02-22 13:44:51 -0500

    Refactor Carousel CSS to styled components

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -1,6 +1,65 @@
 import React, { Component } from 'react';
 import { carouselData } from '../../data'
-// Complete this Carousel 
+import styled, {css} from 'styled-components';
+
+const CarouselWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  height: 500px;
+  position: relative;
+  overflow: hidden;
+  margin-top: 16px;
+
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+
+  img {
+    width: 100%;
+    display: none;
+  }
+`
+
+const CarouselButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: none;
+  flex-direction: row;
+  color: #fff;
+  background-color: #333;
+  font-size: 40px;
+  border-radius: 50%;
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+
+  &:hover {
+    color: #333;
+    background-color: #fff;
+    border: 2px solid #333;
+  }
+
+  ${props =>
+    props.type === 'left' &&
+    css`
+      top: 50%;
+      left: 25px;
+      transform: translate(0, -50%);
+    `}
+
+    ${props =>
+      props.type === 'right' &&
+      css`
+        top: 50%;
+        right: 25px;
+        transform: translate(0, -50%);
+      `}  
+`
+
 export default class Carousel extends Component {
   constructor(props){
     super(props);
@@ -47,11 +106,11 @@ export default class Carousel extends Component {
   
   render(){
     return (
-      <div className="carousel">
-        <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+      <CarouselWrapper>
+        <CarouselButton type="left" onClick={this.leftClick}>{"<"}</CarouselButton>
         <div>{this.selectedImage()}</div>
-        <div className="right-button" onClick={this.rightClick}>{">"}</div>
-      </div>
+        <CarouselButton type="right" onClick={this.rightClick}>{">"}</CarouselButton>
+      </CarouselWrapper>
     )
   }
 }
