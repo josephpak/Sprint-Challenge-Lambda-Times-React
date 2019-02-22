45742bdfae4f06a7e4e5adba03eacb963832e568
joepak <josephpak2014@gmail.com>
2019-02-22 14:22:07 -0500

    App styled components refactor

--- lambdatimes/src/App.js
+++ lambdatimes/src/App.js
@@ -2,14 +2,24 @@ import React from 'react';
 import authenticate from './components/authenticate/authenticate'
 import HomePage from './components/HomePage/HomePage';
 import Login from './components/Login/Login'
+import styled from 'styled-components';
 
 const ControlledDisplay = authenticate(HomePage)(Login)
 
+const AppWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  background-color: #f1f1f1;
+  color: #333;
+  font-family: 'PT Sans', sans-serif;
+`
+
 const App = () => {
   return (
-    <div className="App">
+    <AppWrapper>
       <ControlledDisplay />
-    </div>
+    </AppWrapper>
   );
 }
 
